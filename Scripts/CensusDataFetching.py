2ec58ba7fb338c85e0aa20ed3e9fd62e39c2e655
jemorgan1000 <jemorgan1000@gmail.com>
2020-04-08 20:36:38 -0400

    Added Wills stuff

---
+++ Scripts/CensusDataFetching.py
@@ -0,0 +1,12 @@
+"""
+This file fetches the census data for each Georgia County, removes useless columns, and formats it.
+"""
+
+
+import requests
+import pandas as pd
+import json
+
+base_api = ""
+KEY = 
+
